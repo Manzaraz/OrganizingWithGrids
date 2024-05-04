c040053a9bcf87dc60f5fc692a58f82d07b32e82
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-04 19:27:52 -0300

    Initial Commit

---
+++ OrganizingWithGrids/OrganizingWithGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizingWithGridsApp.swift
+//  OrganizingWithGrids
+//
+//  Created by Christian Manzaraz on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct OrganizingWithGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
